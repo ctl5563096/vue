6a6cb89197ff5a68420fb66346ba794aa0733ff8
shy <chentulin@163.com>
2020-07-01 16:41:51 +0800

    初始化项目

---
+++ global/global.js
@@ -0,0 +1,8 @@
+const baseApiAddress = 'http://127.0.0.1/api';
+
+const apiUrl = {
+    // user api
+    userApi : baseApiAddress + '/userInfo',
+};
+
+export default apiUrl;
---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1 v-if="awesome" @click="test">Vue is awesome!</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data (){
+    return {
+      awesome :true,
+      flag : false
+    }
+  },
+  methods : {
+  test()
+  {
+    let self = this
+    if(this.flag === false){
+      self.$message.success("成功！");
+      self.flag = true
+    }
+  }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
